9db6cf40fda3668713af14c5f1af958c39283ae5
s-takahashijp <shallow.e1621@gmail.com>
2025-05-31 22:25:01 +0900

    second

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_WEB_APP_URL = 'https://s-takahashijp.github.io/reservation-site'; // ★ここをあなたのGASウェブアプリのURLに置き換える★
+const GAS_WEB_APP_URL = 'AKfycbwqJY9XIEQpWO0bg73gNrb_LSFwYuPhiKbP2JrUXK2k'; // ★ここをあなたのGASウェブアプリのURLに置き換える★
 
 const loadingElement = document.getElementById('loading');
 const reservationListElement = document.getElementById('reservation-list');
